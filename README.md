59bfb1601966fd00f67bbced5a0391a44532be33
Kevin Le <kevin@Kevins-MacBook-Air.local>
2023-07-27 18:07:58 -0700

    map first testing

---
+++ README.md
@@ -0,0 +1,6 @@
+yarn add react react-dom leaflet
+yarn add react-leaflet
+yarn add -D @types/leaflet
+
+https://react-leaflet.js.org/docs/start-introduction/
+Testing for now!
---
+++ openstreetmap/src/App.tsx
@@ -0,0 +1,16 @@
+
+import Map from "./Map"
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Map></Map>
+      <div className="text-container"></div>
+    </>
+  );
+}
+
+export default App
