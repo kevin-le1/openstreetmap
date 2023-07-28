04ac72e969dec99182f357b5f9dcd260996fee8a
Kevin Le <117415875+kevin-le1@users.noreply.github.com>
2023-07-27 19:42:52 -0700

    Update Map.tsx

--- openstreetmap/src/Map.tsx
+++ openstreetmap/src/Map.tsx
@@ -10,21 +10,20 @@ export default function Map() {
 
     const position = [35.861660, 104.195396]
     return(
-        <div className='map-container'>
+    <div className='map-container'>
     <div className="map-box">
-<MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: '100%', height:'100%'}}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-      Latitude : {position[0]}, Longitude : {position[1]}
-
-      </Popup>
-    </Marker>
-  </MapContainer>
-  </div>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: '100%', height:'100%'}}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+          Latitude : {position[0]}, Longitude : {position[1]}
+          </Popup>
+        </Marker>
+        </MapContainer>
+    </div>
     </div>
     )
 }
